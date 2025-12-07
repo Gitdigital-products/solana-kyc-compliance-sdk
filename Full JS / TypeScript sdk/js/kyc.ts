188021e539af151ff35146cd30ff6d5b73bbaf4b
Richard Kindler <ricks.online.businesses425@gmail.com>
2025-12-06 19:35:16 -0700

    Create Full JS / TypeScript sdk/js/kyc.ts

---
+++ Full JS / TypeScript sdk/js/kyc.ts
@@ -0,0 +1,11 @@
+import { PublicKey } from "@solana/web3.js";
+ 
+export const REGISTRY_PROGRAM_ID = new PublicKey(
+  "REG1stry11111111111111111111111111111111111"
+);
+ 
+export function attestationPda(wallet: PublicKey) {
+  return PublicKey.findProgramAddressSync(
+    [Buffer.from("attestation"), wallet.toBuffer()],
+    REGISTRY_PROGRAM_ID
+  )[0];
